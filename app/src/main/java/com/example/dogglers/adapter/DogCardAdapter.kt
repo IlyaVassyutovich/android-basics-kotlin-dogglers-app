68d91b8c1d9c46fb12e605ca6d5cce8c15460838
Ilya Vassyutovich <vassyutovich@mindbox.ru>
2022-08-17 12:39:54 +0300

    Implement recycler view adapter and view items

--- app/src/main/java/com/example/dogglers/adapter/DogCardAdapter.kt
+++ app/src/main/java/com/example/dogglers/adapter/DogCardAdapter.kt
@@ -16,51 +16,65 @@
 package com.example.dogglers.adapter
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.dogglers.R
+import com.example.dogglers.const.Layout
+import com.example.dogglers.data.DataSource
+import com.example.dogglers.model.Dog
 
 /**
  * Adapter to inflate the appropriate list item layout and populate the view with information
  * from the appropriate data source
  */
 class DogCardAdapter(
-    private val context: Context?,
+    private val context: Context,
     private val layout: Int
-): RecyclerView.Adapter<DogCardAdapter.DogCardViewHolder>() {
-
-    // TODO: Initialize the data using the List found in data/DataSource
+) : RecyclerView.Adapter<DogCardAdapter.DogCardViewHolder>() {
+    private val _dogs = DataSource.dogs
 
     /**
      * Initialize view elements
      */
-    class DogCardViewHolder(view: View?): RecyclerView.ViewHolder(view!!) {
-        // TODO: Declare and initialize all of the list item UI components
+    class DogCardViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        private val imageDog = view.findViewById<ImageView>(R.id.image_dog)
+        private val textDogName = view.findViewById<TextView>(R.id.text_dog_name)
+        private val textDogAge = view.findViewById<TextView>(R.id.text_dog_age)
+        private val textDogHobbies = view.findViewById<TextView>(R.id.text_dog_hobbies)
+
+        fun setDogData(dog: Dog) {
+            imageDog.setImageResource(dog.imageResourceId)
+            textDogName.text = dog.name
+            // TODO: Extract formatted string into resources somehow
+            textDogAge.text = "Age: ${dog.age}"
+            textDogHobbies.text = "Hobbies: ${dog.hobbies}"
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DogCardViewHolder {
-        // TODO: Use a conditional to determine the layout type and set it accordingly.
-        //  if the layout variable is Layout.GRID the grid list item should be used. Otherwise the
-        //  the vertical/horizontal list item should be used.
+        val layoutId = when (layout) {
+            Layout.GRID -> R.layout.grid_list_item
+            Layout.VERTICAL -> R.layout.vertical_horizontal_list_item
+            Layout.HORIZONTAL -> R.layout.vertical_horizontal_list_item
+            else -> throw RuntimeException("Unknown layout selection ($layout).")
+        }
 
-        // TODO Inflate the layout
-
-        // TODO: Null should not be passed into the view holder. This should be updated to reflect
-        //  the inflated layout.
-        return DogCardViewHolder(null)
+        return parent.context
+            .let { LayoutInflater.from(it) }
+            .inflate(layoutId, parent, false)
+            .let { DogCardViewHolder(it) }
     }
 
-    override fun getItemCount(): Int = 0 // TODO: return the size of the data set instead of 0
+    override fun getItemCount(): Int {
+        return _dogs.size
+    }
 
     override fun onBindViewHolder(holder: DogCardViewHolder, position: Int) {
-        // TODO: Get the data at the current position
-        // TODO: Set the image resource for the current dog
-        // TODO: Set the text for the current dog's name
-        // TODO: Set the text for the current dog's age
-        val resources = context?.resources
-        // TODO: Set the text for the current dog's hobbies by passing the hobbies to the
-        //  R.string.dog_hobbies string constant.
-        //  Passing an argument to the string resource looks like:
-        //  resources?.getString(R.string.dog_hobbies, dog.hobbies)
+        val currentDog = _dogs[position]
+        holder.setDogData(currentDog)
     }
 }
